3257c51522e5ad52177621bda38d40a7b461b202
paintofheart <76064707+ristovska1606@users.noreply.github.com>
2023-03-22 11:19:03 +0100

    updated project

--- ELibraryApp/src/main/java/mk/com/ukim/finki/elibraryapp/ELibraryAppApplication.java
+++ ELibraryApp/src/main/java/mk/com/ukim/finki/elibraryapp/ELibraryAppApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ELibraryAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ELibraryAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ELibraryAppApplication.class, args);
+    }
 
 }
