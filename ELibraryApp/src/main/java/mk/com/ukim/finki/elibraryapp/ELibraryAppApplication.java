10bd13c9f88f98d649dd5646c5f7041bf8c827a3
paintofheart <76064707+ristovska1606@users.noreply.github.com>
2023-03-22 11:13:31 +0100

    created project

---
+++ ELibraryApp/src/main/java/mk/com/ukim/finki/elibraryapp/ELibraryAppApplication.java
@@ -0,0 +1,13 @@
+package mk.com.ukim.finki.elibraryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELibraryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELibraryAppApplication.class, args);
+	}
+
+}
---
+++ ELibraryApp/src/test/java/mk/com/ukim/finki/elibraryapp/ELibraryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.com.ukim.finki.elibraryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELibraryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
