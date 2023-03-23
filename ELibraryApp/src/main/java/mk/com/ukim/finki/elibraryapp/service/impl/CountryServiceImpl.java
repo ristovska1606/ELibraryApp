949a235644a9018503539dc5a7a3ad6bb33bbd52
Jana Ristovska <76064707+ristovska1606@users.noreply.github.com>
2023-03-23 17:41:30 +0100

    implemented country service

---
+++ ELibraryApp/src/main/java/mk/com/ukim/finki/elibraryapp/service/impl/CountryServiceImpl.java
@@ -0,0 +1,65 @@
+package mk.com.ukim.finki.elibraryapp.service.impl;
+
+import mk.com.ukim.finki.elibraryapp.model.Country;
+import mk.com.ukim.finki.elibraryapp.model.dto.CountryDto;
+import mk.com.ukim.finki.elibraryapp.model.exceptions.CountryNotFoundException;
+import mk.com.ukim.finki.elibraryapp.repository.CountryRepository;
+import mk.com.ukim.finki.elibraryapp.service.CountryService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CountryServiceImpl implements CountryService {
+
+    private final CountryRepository countryRepository;
+
+    public CountryServiceImpl(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+
+    @Override
+    public List<Country> findAll() {
+        return this.countryRepository.findAll();
+    }
+
+    @Override
+    public Optional<Country> findById(Long id) {
+        Country country = this.countryRepository.findById(id).orElseThrow(() -> new CountryNotFoundException());
+        return Optional.of(country);
+    }
+
+    @Override
+    public Optional<Country> save(String name, String continent) {
+        Country country = new Country(name, continent);
+        return Optional.of(country);
+    }
+
+    @Override
+    public Optional<Country> save(CountryDto countryDto) {
+        Country country = new Country(countryDto.getName(), countryDto.getContinent());
+        return Optional.of(country);
+    }
+
+    @Override
+    public Optional<Country> edit(Long id, String name, String continent) {
+        Country country = this.countryRepository.findById(id).orElseThrow(() -> new CountryNotFoundException());
+        country.setName(name);
+        country.setContinent(continent);
+        return Optional.of(country);
+    }
+
+    @Override
+    public Optional<Country> edit(Long id, CountryDto countryDto) {
+        Country country = this.countryRepository.findById(id).orElseThrow(() -> new CountryNotFoundException());
+        country.setName(countryDto.getName());
+        country.setContinent(countryDto.getContinent());
+        return Optional.of(country);
+    }
+
+    @Override
+    public void delete(Long id) {
+        this.countryRepository.deleteById(id);
+    }
+}
